0579b2577b63dab2236013fe748d693e910027a1
saifulislam11 <saifulislambuet11@gmail.com>
2020-10-22 01:52:53 +0600

    few styles added

--- homeApp/urls.py
+++ homeApp/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+###registering appname to hyperlink in htmls
+app_name = 'homeApp'
+
+
 urlpatterns = [
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path("aboutus",views.aboutus,name="aboutus"),
+    path("contactus",views.contactus,name="contactus")
     
 ]
--- homeApp/views.py
+++ homeApp/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'homeApp/index.html')
+
+def aboutus(request):
+    return render(request,'homeApp/aboutus.html')
+def contactus(request):
+    return render(request,'homeApp/contactus.html')
