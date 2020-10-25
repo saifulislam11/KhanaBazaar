187195819985ff157bcdd763b19111d92d8eaa8c
saifulislam11 <saifulislambuet11@gmail.com>
2020-10-25 20:49:24 +0600

    search restaurant activated & few styles added

--- homeApp/views.py
+++ homeApp/views.py
@@ -3,31 +3,8 @@ import cx_Oracle
 con = cx_Oracle.connect("KB", "123", "localhost/orcl")
 print("Connected!")
 c = con.cursor()
-c.execute('select * from RESTAURANT')
-dict_result = []
-for row in c:
-    id=row[0]
-    name = row[1]
-    path=row[3]
-    dic = {'id':id,'name':name,'path':path}
-    dict_result.append(dic)
-list1=[]
-list2=[]
-list3=[]
-brk=int(len(dict_result)/3)
-for i in range(len(dict_result)):
-    if(int(i/brk) == 0):
-        list1.append(dict_result[i])
-    elif(int(i/brk) == 1):
-        list2.append(dict_result[i])
-    else:
-        list3.append(dict_result[i])
-print(list3)
 # Create your views here.
 def index(request):
-    con = cx_Oracle.connect("KB", "123", "localhost/orcl")
-    print("Connected!")
-    c = con.cursor()
     c.execute('select * from RESTAURANT')
     dict_result = []
     for row in c:
@@ -59,4 +36,19 @@ def aboutus(request):
 def contactus(request):
     return render(request,'homeApp/contactus.html')
 def restaurant(request):
-    return render(request,'homeApp/restaurant.html')
+    c.execute('select * from RESTAURANT')
+    query=None
+    results=None
+    if request.method == 'POST':
+        query = request.POST.get('restaurant')
+        print(query)
+        for row in c:
+            print(row[1])
+            if str(row[1].lower()).find(str(query.lower()))!=-1:
+                results=row[3]
+                print(row[3])
+    
+    
+
+
+    return render(request,'homeApp/restaurant.html',{'path':results})
