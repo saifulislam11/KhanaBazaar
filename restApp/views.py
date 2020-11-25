27e654f5c741e7de2b795c8da2544d5a1582966f
vodro <vdrkowshic@gmail.com>
2020-11-25 23:01:32 +0600

    adding a food item works

--- restApp/views.py
+++ restApp/views.py
@@ -2,10 +2,10 @@ from django.contrib import messages
 from django.shortcuts import render
 
 from helper import sql
+from helper.read_write_to_file import handle_uploaded_file
 from helper.wrap_and_encode import get_hashed_value, wrap_with_in_single_quote
-
-
 # Create your views here.
+from khanabazaar.settings import STATIC_ROOT, IMAGE_PATH
 
 
 def index(request):
@@ -45,14 +45,78 @@ def index(request):
             request.session.flush()
             print('log out ing')
             return render(request, 'restApp/sign_in.html', context)
-        return render(request, 'restApp/sign_in.html', context)
+
+        if not request.session.is_empty():
+            return render(request, 'restApp/index.html', context)
+        else:
+            return render(request, 'restApp/sign_in.html', context)
 
     return render(request, 'restApp/sign_in.html', context)
 
 
 def add_food(request):
     context = {}
-    return render(request, 'restApp/sign_in.html', context)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        image = request.FILES['food_image']
+        price = request.POST.get('price')
+        description = request.POST.get('description')
+        food_id = sql.get_next_id()
+        rest_id = request.session.get('id')
+        Type = request.POST.get('type')
+        image_id = 0
+        # this are default
+        available = 'N'
+        offer = 0
+        food_image_path = None
+
+        try:
+            conn = sql.open_connection()
+            cursor = conn.cursor()
+            # first add to fooditem table
+            to_execute = "INSERT INTO FOOD_ITEM(ID, RESTAURANT_ID, NAME, PRICE,OFFER, AVAILIBILITY, DESCRIPTION, TYPE) " \
+                         "VALUES ({id}, {rest_id}, {name},{price}, {offer}, {availibiliy}, {description}, {type} )"
+            to_execute = to_execute.format(
+                id=wrap_with_in_single_quote(food_id),
+                rest_id=wrap_with_in_single_quote(rest_id),
+                name=wrap_with_in_single_quote(name),
+                price=price,
+                offer=offer,
+                availibiliy=wrap_with_in_single_quote(available),
+                description=wrap_with_in_single_quote(description),
+                type=wrap_with_in_single_quote(Type)
+            )
+            print(to_execute)
+            cursor.execute(to_execute)
+
+            # now add to food item path
+            if image is not None:
+                image_id = sql.get_next_id()
+                food_image_path = 'food' + image_id + '.jpg'
+                handle_uploaded_file(image, food_image_path, STATIC_ROOT + '/img')
+                handle_uploaded_file(image, food_image_path, IMAGE_PATH + '/img/')
+            else:
+                food_image_path = 'food' + image_id + '.jpg'
+
+            to_execute = "INSERT INTO FOOD_ITEM_PATH(ID, IMAGE_PATH) VALUES ( {id}, {food_path})"
+            to_execute = to_execute.format(
+                id=wrap_with_in_single_quote(food_id),
+                food_path=wrap_with_in_single_quote(food_image_path)
+            )
+            cursor.execute(to_execute)
+            cursor.close()
+            conn.commit()
+            messages.info(request, 'Successfully added Food')
+
+        except Exception as e:
+            print(e)
+            print("something is wrong in adding food")
+            print(e)
+            conn.rollback()
+        finally:
+            conn.close()
+
+    return render(request, 'restApp/add_food.html', context)
 
 
 def edit_food(request):
