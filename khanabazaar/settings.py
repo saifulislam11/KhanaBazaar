f887d8ca3533c441ef9ceffaa96bdac6b740729a
vodro <vdrkowshic@gmail.com>
2020-11-25 16:38:18 +0600

    restApp and foodmanApp added

--- khanabazaar/settings.py
+++ khanabazaar/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'homeApp',
     'adminApp',
+    'restApp',
+    'foodmanApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
