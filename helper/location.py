544ccde4690284bf566bd60579505292b7450707
vodro <vdrkowshic@gmail.com>
2020-11-30 18:57:45 +0600

    location helper handles location related stuffs

---
+++ helper/location.py
@@ -0,0 +1,18 @@
+buet_location = '23.7265768,90.3926623'
+
+
+def valid_location(location):
+    '''
+    checks if a loction is valid or not
+    :param loc:
+    :return:
+    '''
+    try:
+        arr = location.split('')
+        if arr.size() != 2:
+            raise Exception
+        a = float(arr[0])
+        b = float(arr[1])
+    except Exception as e:
+        return False
+    return True
