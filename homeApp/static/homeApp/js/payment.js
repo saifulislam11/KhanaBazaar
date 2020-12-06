b1e6ae14234302dbf82be70eb8420e6a25b3398a
saifulislam11 <saifulislambuet11@gmail.com>
2020-12-07 01:05:14 +0600

    customer current location added

--- homeApp/static/homeApp/js/payment.js
+++ homeApp/static/homeApp/js/payment.js
@@ -1,6 +1,72 @@
     //------------VARIABLES FOR PAYMENT PAGE----------//
 
 document.addEventListener('DOMContentLoaded',function(){
+
+
+    ///---------------location--------------///
+    const latlng_buet = {'lat': 23.7265768, 'lng': 90.3926623};
+    var loction = document.getElementById('location');
+
+    function splitter(ss) {
+        var arr = ss.split(',');
+        var a = +arr[0];
+        var b = +arr[1];
+        return {'lat': a, 'lng': b};
+    }
+
+
+    function parser_haversine(ss) {
+        return {
+            latitude: ss[0],
+            longitude: ss[1]
+        }
+    }
+    var map = L.map('map',
+        {
+            center: latlng_buet,
+            enableHighAccuracy: true
+        });
+    var cur_loc_marker = L.marker(splitter(loction.value)).addTo(map);
+    //loction.value = ""
+    cur_loc_marker.bindPopup("Your current Location").openPopup();
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1Ijoidm9kcm8iLCJhIjoiY2tpMzh1ZnZ5MGRwazJyc3k1d2c2dmcxOSJ9.EeruKONlBRsXX8I77GbuPw'
+    }).addTo(map);
+
+
+    map.locate({
+        enableHighAccuracy: true,
+        watch: true,
+        setView: true,
+        maxZoom: 18
+    });
+
+    function onLocationFound(e) {
+        /*
+        var dist = haversine(cur_loc_marker.getLatLng(), e.latlng, {
+            format: '{lat,lng}',
+            'unit': 'meter'
+        });
+        if (dist > 500) {
+            loction.value = e.latlng.lat + ',' + e.latlng.lng;
+            console.log(dist);
+            alert('Your Location is being Updated');
+            update_location_form.submit()
+        }*/
+
+        cur_loc_marker.setLatLng(e.latlng);
+        map.flyTo(e.latlng);
+        console.log('map created with current location');
+    }
+
+    map.on('locationfound', onLocationFound);
+
+    ///---------------end of location--------------//
     const PcartBTN = document.querySelector('.Pcart-btn');
     const PcloseCartBTN = document.querySelector('.Pclose-cart');
     const PclearCartBTN = document.querySelector('.Pclear-cart');
@@ -230,6 +296,7 @@ close.addEventListener("click",function(){
                 temp_fixed = temp_fixed.replace('$','');
                 temp_percent = temp_percent.replace('%','');
                 var remainig_promo = promo_table_row[i].children[6].innerHTML;
+                
                 if(Number(temp_percent) === 0){
                     if(Number(remainig_promo)===0)
                     {
