960c363d196716a120e8fb02fd34eeb129f66ab0
vodro <vdrkowshic@gmail.com>
2020-12-08 16:25:17 +0600

    add foodman works now with function

--- adminApp/views.py
+++ adminApp/views.py
@@ -17,55 +17,61 @@ from khanabazaar.settings import IMAGE_PATH, STATIC_ROOT
 
 def index(request):
     context = {}
-    return_value = None
     not_this_season(request, app_name)
     if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
-        password = get_hashed_value(password)
-        # print(email, password)
-        c = sql.create_cursor()
-        to_execute = "Select * From ADMIN Where EMAIL = {email} " \
-                     "and PASSWORD_HASH = {password}"
-        to_execute = to_execute.format(
-            email=wrap_with_in_single_quote(email),
-            password=wrap_with_in_single_quote(password)
-        )
-        c.execute(to_execute)
-        admin = c.fetchone()
-        c.close()
+        try:
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            password = get_hashed_value(password)
+            c = sql.create_cursor()
+            to_execute = "Select * From ADMIN Where EMAIL = {email} " \
+                         "and PASSWORD_HASH = {password}"
+            to_execute = to_execute.format(
+                email=wrap_with_in_single_quote(email),
+                password=wrap_with_in_single_quote(password)
+            )
+            c.execute(to_execute)
+            admin = c.fetchone()
 
-        if admin is None:
-            return render(request, 'adminApp/sign_in.html', context)
-        else:
-            # print('We need to do something')
-            id = admin[0]
-            last_name = admin[1]
-            first_name = admin[2]
-            request.session['id'] = id
-            request.session['last_name'] = last_name
-            request.session['first_name'] = first_name
-            request.session['email'] = email
-            request.session['app_name'] = app_name
-            context['user_name'] = last_name
+            if admin is None:
+                messages.info(request, "Incorrect credentials")
+                return render(request, 'adminApp/sign_in.html', context)
+            else:
+                id = admin[0]
+                last_name = admin[1]
+                first_name = admin[2]
+                request.session['id'] = id
+                request.session['last_name'] = last_name
+                request.session['first_name'] = first_name
+                request.session['email'] = email
+                request.session['app_name'] = app_name
+                context['user_name'] = last_name
+                return render(request, 'adminApp/index.html', context)
+        except Exception as e:
+            print(e)
+            messages.info(request, "Some unknown error occured!")
             return render(request, 'adminApp/index.html', context)
+        finally:
+            c.close()
+            pass
 
     elif request.method == 'GET':
+        try:
+            action = request.GET.get('action')
+            if action == 'logout':
+                if not request.session.is_empty():
+                    request.session.flush()
+                    messages.info(request, 'You have successfully Log out!')
+                    return render(request, 'adminApp/sign_in.html', context)
 
-        action = request.GET.get('action')
-        # print('action is ' + str(action))
-        # if action is not None:
-        # request.GET.pop('action')
-
-        if action == 'logout':
             if not request.session.is_empty():
-                request.session.flush()
-                print('logout ing')
+                context['user_name'] = request.session.get('last_name')
+                return render(request, 'adminApp/index.html', context)
+            else:
                 return render(request, 'adminApp/sign_in.html', context)
-        if not request.session.is_empty():
-            context['user_name'] = request.session.get('last_name')
-            return render(request, 'adminApp/index.html', context)
-        else:
+        except Exception as e:
+            print(e)
+            messages.info(request, "Some unknown error occured!")
             return render(request, 'adminApp/sign_in.html', context)
 
 
@@ -76,52 +82,60 @@ def add_restaurant(request):
         messages.info(request, 'YOU ARE NOT LOGGED IN ')
         return redirect('/admin')
     if request.method == 'POST':
-        name = request.POST.get('name')
-        location = request.POST.get('location')
-        logo = request.FILES.get('restaurantLogo')
-        email = request.POST.get('email')
-        open_time = request.POST.get('openTime')
-        close_time = request.POST.get('closeTime')
-        password1 = request.POST.get('password1')
-        password1 = get_hashed_value(password1)
-        password2 = request.POST.get('password2')
-        logo_path = None
-        id = get_next_id()
-        if logo is not None:
-            logo_path = 'rest' + id + '.' + 'jpg'
-            handle_uploaded_file(logo, logo_path, IMAGE_PATH + '/img/')
-            handle_uploaded_file(logo, logo_path, STATIC_ROOT + '/img/')
-            # collectstatic()
-        else:
-            logo_path = 'rest0.jpg'  # we will use rest0 as a default restaurant pic
-        to_execute = "INSERT INTO RESTAURANT(ID,NAME,LOCATION,LOGO_PATH,RATING,OPEN_TIME,CLOSE_TIME,EMAIL,PASSWORD_HASH)" \
-                     "VALUES({id}, {name}, {location}, {logo_path}, {rating}, {open_time}, {close_time}, {email}, " \
-                     "{password_hash}) "
-        to_execute = to_execute.format(
-            id=wrap_with_in_single_quote(id),
-            name=wrap_with_in_single_quote(name),
-            location=wrap_with_in_single_quote(location),
-            logo_path=wrap_with_in_single_quote(logo_path),
-            rating=5.00,
-            open_time=wrap_with_in_single_quote(open_time),
-            close_time=wrap_with_in_single_quote(close_time),
-            email=wrap_with_in_single_quote(email),
-            password_hash=wrap_with_in_single_quote(password1)
-        )
-        # print(wrap_with_in_single_quote(email))
-        # print(to_execute)
-        sql.execute(to_execute)
+        try:
+            name = request.POST.get('name')
+            location = request.POST.get('location')
+            logo = request.FILES.get('restaurantLogo')
+            email = request.POST.get('email')
+            open_time = request.POST.get('openTime')
+            close_time = request.POST.get('closeTime')
+            password1 = request.POST.get('password1')
+            password1 = get_hashed_value(password1)
+            password2 = request.POST.get('password2')
+            logo_path = None
+            id = get_next_id()
+            if logo is not None:
+                logo_path = 'rest' + id + '.' + 'jpg'
+                handle_uploaded_file(logo, logo_path, IMAGE_PATH + '/img/')
+                handle_uploaded_file(logo, logo_path, STATIC_ROOT + '/img/')
+                # collectstatic()
+            else:
+                logo_path = 'rest0.jpg'  # we will use rest0 as a default restaurant pic
+            to_execute = "INSERT INTO RESTAURANT(ID,NAME,LOCATION,LOGO_PATH,RATING,OPEN_TIME,CLOSE_TIME,EMAIL,PASSWORD_HASH)" \
+                         "VALUES({id}, {name}, {location}, {logo_path}, {rating}, {open_time}, {close_time}, {email}, " \
+                         "{password_hash}) "
+            to_execute = to_execute.format(
+                id=wrap_with_in_single_quote(id),
+                name=wrap_with_in_single_quote(name),
+                location=wrap_with_in_single_quote(location),
+                logo_path=wrap_with_in_single_quote(logo_path),
+                rating=5.00,
+                open_time=wrap_with_in_single_quote(open_time),
+                close_time=wrap_with_in_single_quote(close_time),
+                email=wrap_with_in_single_quote(email),
+                password_hash=wrap_with_in_single_quote(password1)
+            )
+            # print(wrap_with_in_single_quote(email))
+            # print(to_execute)
+            sql.execute(to_execute)
 
-        # Now adding into relation manages
-        to_execute = "INSERT INTO MANAGES(RESTAURANT_ID, ADMIN_ID) " \
-                     "VALUES({restaurant_ID}, {admin_ID})"
-        to_execute = to_execute.format(
-            restaurant_ID=wrap_with_in_single_quote(id),
-            admin_ID=wrap_with_in_single_quote(request.session.get('id')))
-        # print(to_execute)
-        sql.execute(to_execute)
-        messages.info(request, 'Adding Done')
-        return redirect('/admin/add_restaurant')
+            # Now adding into relation manages
+            to_execute = "INSERT INTO MANAGES(RESTAURANT_ID, ADMIN_ID) " \
+                         "VALUES({restaurant_ID}, {admin_ID})"
+            to_execute = to_execute.format(
+                restaurant_ID=wrap_with_in_single_quote(id),
+                admin_ID=wrap_with_in_single_quote(request.session.get('id')))
+            # print(to_execute)
+            sql.execute(to_execute)
+            messages.info(request, 'Restaurant has been successfully added')
+            return redirect('/admin/add_restaurant')
+        except Exception as e:
+            print(e)
+            messages.info(request, 'restaurant was not added')
+            return redirect('/admin/add_restaurant')
+        finally:
+            pass
+
     return render(request, 'adminApp/add_restaurant.html', context)
 
 
@@ -153,8 +167,6 @@ def add_food_man(request):
         profile_img = request.FILES.get('profile_img')
         profile_img_path = None
         foodman_id = get_next_id()
-        contract_id = get_next_id()
-        vehicle_id = get_next_id()
         if profile_img is not None:
             profile_img_path = 'foodman' + foodman_id + '.' + 'jpg'
             handle_uploaded_file(profile_img, profile_img_path, IMAGE_PATH + '/img/')
@@ -166,73 +178,77 @@ def add_food_man(request):
 
         try:
             # add to foodman table
-            to_execute = "INSERT INTO FOODMAN(ID, NAME, EMAIL, PASSWORD_HASH, RATING, IMAGE_PATH) " \
-                         " VALUES({id}, {name}, {email},{password}, {rating},{profile_img_path})"
-            to_execute = to_execute.format(
-                id=wrap_with_in_single_quote(foodman_id),
-                name=wrap_with_in_single_quote(name),
-                email=wrap_with_in_single_quote(email),
-                password=wrap_with_in_single_quote(password1),
-                rating=wrap_with_in_single_quote(init_rating),
-                profile_img_path=wrap_with_in_single_quote(profile_img_path)
-            )
-            print(to_execute)
-            cursor.execute(to_execute)
-            # add phone no
-            to_execute = "INSERT INTO FOODMAN_PHONE(ID,PHONE_NO) " \
-                         " VALUES({foodman_id},{phone})"
-            to_execute = to_execute.format(
-                foodman_id=wrap_with_in_single_quote(foodman_id),
-                phone=wrap_with_in_single_quote(phone1)
-            )
-            print(to_execute)
-            cursor.execute(to_execute)
-
-            # Insert into vehicle table
-            to_execute = "INSERT INTO VEHICLE(ID, REG_NO, TYPE) " \
-                         " VALUES({vehicle_id}, {reg_no}, {type})"
-            to_execute = to_execute.format(
-                vehicle_id=wrap_with_in_single_quote(vehicle_id),
-                reg_no=wrap_with_in_single_quote(reg_no),
-                type=wrap_with_in_single_quote(vehicle_type)
-            )
-            print(to_execute)
-            cursor.execute(to_execute)
-            # Insert into DELIVERS_BY table
-            to_execute = "INSERT INTO DELIVERS_BY(FOODMAN_ID, VEHICLE_ID) " \
-                         " VALUES({foodman_id}, {vehicle_id})"
-            to_execute = to_execute.format(
-                foodman_id=wrap_with_in_single_quote(foodman_id),
-                vehicle_id=wrap_with_in_single_quote(vehicle_id)
-            )
-            print(to_execute)
-            cursor.execute(to_execute)
-            # Insert into CONTRACT table
-            to_execute = "INSERT INTO CONTRACT(ID,DURATION, SALARY, INIT_RATING, DATE_OF_CONTRACT) " \
-                         " VALUES({contract_id},{duration}, {salary},{init_rating}, SYSDATE)"
-            to_execute = to_execute.format(
-                contract_id=wrap_with_in_single_quote(contract_id),
-                duration=wrap_with_in_single_quote(duration),
-                salary=wrap_with_in_single_quote(salary),
-                init_rating=init_rating
-            )
-            cursor.execute(to_execute)
-
-            # Insert into APPOINTS table
-            to_execute = "INSERT INTO APPOINTS(FOODMAN_ID, ADMIN_ID, CONTRACT_ID) " \
-                         " VALUES({foodman_id}, {admin_id}, {contract_id})"
-            to_execute = to_execute.format(
-                foodman_id=wrap_with_in_single_quote(foodman_id),
-                admin_id=wrap_with_in_single_quote(request.session.get('id')),
-                contract_id=wrap_with_in_single_quote(contract_id)
-            )
-            print(to_execute)
-            cursor.execute(to_execute)
+            # to_execute = "INSERT INTO FOODMAN(ID, NAME, EMAIL, PASSWORD_HASH, RATING, IMAGE_PATH) " \
+            #              " VALUES({id}, {name}, {email},{password}, {rating},{profile_img_path})"
+            # to_execute = to_execute.format(
+            #     id=wrap_with_in_single_quote(foodman_id),
+            #     name=wrap_with_in_single_quote(name),
+            #     email=wrap_with_in_single_quote(email),
+            #     password=wrap_with_in_single_quote(password1),
+            #     rating=wrap_with_in_single_quote(init_rating),
+            #     profile_img_path=wrap_with_in_single_quote(profile_img_path)
+            # )
+            # print(to_execute)
+            # cursor.execute(to_execute)
+            # # add phone no
+            # to_execute = "INSERT INTO FOODMAN_PHONE(ID,PHONE_NO) " \
+            #              " VALUES({foodman_id},{phone})"
+            # to_execute = to_execute.format(
+            #     foodman_id=wrap_with_in_single_quote(foodman_id),
+            #     phone=wrap_with_in_single_quote(phone1)
+            # )
+            # print(to_execute)
+            # cursor.execute(to_execute)
+            #
+            # # Insert into vehicle table
+            # to_execute = "INSERT INTO VEHICLE(ID, REG_NO, TYPE) " \
+            #              " VALUES({vehicle_id}, {reg_no}, {type})"
+            # to_execute = to_execute.format(
+            #     vehicle_id=wrap_with_in_single_quote(vehicle_id),
+            #     reg_no=wrap_with_in_single_quote(reg_no),
+            #     type=wrap_with_in_single_quote(vehicle_type)
+            # )
+            # print(to_execute)
+            # cursor.execute(to_execute)
+            # # Insert into DELIVERS_BY table
+            # to_execute = "INSERT INTO DELIVERS_BY(FOODMAN_ID, VEHICLE_ID) " \
+            #              " VALUES({foodman_id}, {vehicle_id})"
+            # to_execute = to_execute.format(
+            #     foodman_id=wrap_with_in_single_quote(foodman_id),
+            #     vehicle_id=wrap_with_in_single_quote(vehicle_id)
+            # )
+            # print(to_execute)
+            # cursor.execute(to_execute)
+            # # Insert into CONTRACT table
+            # to_execute = "INSERT INTO CONTRACT(ID,DURATION, SALARY, INIT_RATING, DATE_OF_CONTRACT) " \
+            #              " VALUES({contract_id},{duration}, {salary},{init_rating}, SYSDATE)"
+            # to_execute = to_execute.format(
+            #     contract_id=wrap_with_in_single_quote(contract_id),
+            #     duration=wrap_with_in_single_quote(duration),
+            #     salary=wrap_with_in_single_quote(salary),
+            #     init_rating=init_rating
+            # )
+            # cursor.execute(to_execute)
+            #
+            # # Insert into APPOINTS table
+            # to_execute = "INSERT INTO APPOINTS(FOODMAN_ID, ADMIN_ID, CONTRACT_ID) " \
+            #              " VALUES({foodman_id}, {admin_id}, {contract_id})"
+            # to_execute = to_execute.format(
+            #     foodman_id=wrap_with_in_single_quote(foodman_id),
+            #     admin_id=wrap_with_in_single_quote(request.session.get('id')),
+            #     contract_id=wrap_with_in_single_quote(contract_id)
+            # )
+            # print(to_execute)
+            # cursor.execute(to_execute)
+            cursor = sql.create_cursor()
+            admin_id = request.session.get('id')
+            cursor.callproc('ADD_FOODMAN', [admin_id, foodman_id, name, email, password1, phone1,
+                                           init_rating, profile_img_path, reg_no, vehicle_type, salary, duration])
             messages.info(request, "adding of foodman was successfull")
 
-        except:
+        except Exception as e:
             print("Something is not right. And why rollback is not working.")
-            sql.rollback()
+            print(e)
             messages.info(request, "adding of foodman was unsuccessfull")
 
         finally:
@@ -300,7 +316,7 @@ def offer_promo(request):
         customers = request.POST.get('customers')
         customers = customers.split(',')  # data was send via comma separated strings
         promo_id = request.POST.get('promo')
-        if (promo_id == ''):
+        if promo_id == '':
             messages.info(request, 'please select a promo')
         elif len(customers) == 0:
             messages.info(request, 'You have not selected any customer')
