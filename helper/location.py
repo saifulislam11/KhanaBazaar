3ead1a0d6fb2e655682cbfafeb94f826c8bd0ad2
vodro <vdrkowshic@gmail.com>
2020-12-09 20:45:40 +0600

    location.py splitter added :3

--- helper/location.py
+++ helper/location.py
@@ -8,11 +8,12 @@ def valid_location(location):
     :return:
     '''
     try:
-        arr = location.split('')
+        arr = location.split(',')
         if arr.size() != 2:
             raise Exception
         a = float(arr[0])
         b = float(arr[1])
     except Exception as e:
+        print(e)
         return False
     return True
